621071816d83a6a9afd1181614a6fe40170b98ce
KwonOjin <ojin630@naver.com>
2026-08-04 15:58:39 +0900

    [FEAT] 리액트 라우트를 통한 경로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Main from "./pages/Main.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
 function App() {
   return (
-    <div>
-      <h1>해커톤 프로젝트</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
