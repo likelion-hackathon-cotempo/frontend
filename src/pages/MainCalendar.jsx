49003595270201a94f76d25f0b75eb9c6baa5783
KevinHan142 <131000805+KevinHan142@users.noreply.github.com>
2026-08-19 13:35:06 +0900

    [FEAT] 캘린더 페이지 실제 API 연동

--- src/pages/MainCalendar.jsx
+++ src/pages/MainCalendar.jsx
@@ -2,11 +2,11 @@ import { useOutletContext } from "react-router-dom";
 import CalendarPage from "../components/home/calendar/CalendarPage.jsx";
 
 function MainCalendar() {
-  const { context, teams } = useOutletContext();
+  const { context, teamId, teams } = useOutletContext();
 
   return (
     <main className="min-w-0 flex-1">
-      <CalendarPage context={context} teams={teams} />
+      <CalendarPage context={context} teamId={teamId} teams={teams} />
     </main>
   );
 }
